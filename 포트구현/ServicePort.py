24f34a7d4aaa81c82faf522a896bdb6a7d50710b
GHCats <chitmdgml1004@gmail.com>
2024-02-07 12:19:16 +0900

    edit

--- 포트구현/ServicePort.py
+++ 포트구현/ServicePort.py
@@ -1,11 +1,11 @@
 import imaplib
 import base64
 from pysnmp.hlapi import *
-import logging
+#import logging
 import socket
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+# logging.basicConfig(level=logging.INFO)
+# logger = logging.getLogger(__name__)
 
 def IMAP_conn(host, port, use_ssl=False):   
     try:
@@ -23,26 +23,26 @@ def IMAP_conn(host, port, use_ssl=False):
         pure_banner_info = banner_info_str.split('[')[0]
         encoded_data = banner_info_str.split('[')[1].split(']')[0]
         decoded_data = base64.b64decode(encoded_data)
-        logger.info(f'배너 정보: {pure_banner_info}')
+        print(f'배너 정보: {pure_banner_info}')
        
         # 디코딩된 데이터를 문자열로 변환 (UTF-8 인코딩 사용)
         try:
             decoded_string = decoded_data.decode('utf-8')
-            logger.info(f'생성 서버: {decoded_string}')
+            print(f'생성 서버: {decoded_string}')
         except UnicodeDecodeError:
-            logger.warning("UTF-8 디코딩 실패")
+            print("UTF-8 디코딩 실패\n 생성 서버: {decoded_data}")
 
 
     except imaplib.IMAP4_SSL.error as ssl_error:
-        logger.error("SSL 오류:", ssl_error)
+        print("SSL 오류:", ssl_error)
         return None
 
     except imaplib.IMAP4.error as imap_error:
-        logger.error("IMAP 오류:", imap_error)
+        print("IMAP 오류:", imap_error)
         return None
 
     except Exception as e:
-        logger.error("예기치 않은 오류 발생:", e)
+        print("예기치 않은 오류 발생:", e)
         return None
 
 def SNMP_conn(host):
@@ -63,28 +63,29 @@ def SNMP_conn(host):
             ObjectType(sysname_oid),
             ObjectType(sysdesc_oid)
         )
+        print(f"Connected to SNMP server successfully.")
 
         #요청에 대한 결과 추출
         error_indication, error_status, error_index, var_binds = next(snmp_request)
         
         if error_indication:
-                logger.error(f"에러: {error_indication}")
+                print(f"에러: {error_indication}")
         elif error_status:
-            logger.error(f"에러 상태: {error_status}")
+            print(f"에러 상태: {error_status}")
         else:
             for var_bind in var_binds:
                 if sysname_oid.isPrefixOf(var_bind[0]):
-                    logger.info(f"시스템 이름: {var_bind[1].prettyPrint()}")
+                    print(f"시스템 이름: {var_bind[1].prettyPrint()}")
                 elif sysdesc_oid.isPrefixOf(var_bind[0]):
-                    logger.info(f"시스템 설명: {var_bind[1].prettyPrint()}")
+                    print(f"시스템 설명: {var_bind[1].prettyPrint()}")
     except socket.timeout as timeout_error:
-        logger.error(f"연결 시간 초과: {timeout_error}")
+        print(f"연결 시간 초과: {timeout_error}")
 
     except socket.error as socket_error:
-        logger.error(f"소켓 오류: {socket_error}")
+        print(f"소켓 오류: {socket_error}")
 
     except Exception as e:
-        logger.error("예기치 않은 오류 발생:", e)
+        print("예기치 않은 오류 발생:", e)
 
 if __name__ == '__main__':
     host = '127.0.0.1'
