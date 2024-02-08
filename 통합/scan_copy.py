72818ee4fbf69f6976ecdce622297ef900aa167c
GHCats <chitmdgml1004@gmail.com>
2024-02-08 17:50:29 +0900

    update

--- 통합/scan_copy.py
+++ 통합/scan_copy.py
@@ -7,6 +7,14 @@
 # 동진님 파트 출력 기준이 애매해서 다시 한번 확인하기
 # 현모님이랑 동진님이 25번포트 해주셔서 이 부분 수정하기
 
+# 일단 포트 가져오는 거 성공
+
+# 현모님 코드를 없애보기 25번
+# port: 25
+# 동진님꺼로 25 돌렸을 때 결과
+# state: open
+# error_message: error
+# time = 2.5s
 
 import socket
 import struct
@@ -40,7 +48,7 @@ def SYN_scan(host, port):
         return None  # 예외 발생 시 None 반환
     finally:
         sock.close()
-
+        
 # 동진님이 구현한 방식
 def syn_scan(ip, port):
     packet = IP(dst=ip)/TCP(dport=port, flags="S")
@@ -55,8 +63,8 @@ def syn_scan(ip, port):
                 return False  # 포트 닫힘
     return False  # 응답없거나 다른에러
 
-def udp_scan(host):
-    port = 520
+def udp_scan(host, port):
+    #port = 520
     response_data = {
         'port': port,
         'state': 'open or filterd'
@@ -77,9 +85,8 @@ def udp_scan(host):
         #print(f"Port {port}: Received unexpected response.")
 
 
-def scan_smtp_port(host):
-    port = 587
-    
+def scan_smtp_port(host, port):
+   
     response_data = {
         'port': port,
         'state': 'closed'
@@ -106,8 +113,8 @@ def scan_smtp_port(host):
     return response_data
 
 
-def scan_smtps_port(host):
-    port = 587
+def scan_smtps_port(host, port):
+    #port = 587
     response_data = {
         'port': port,
         'state': 'closed'
@@ -127,8 +134,8 @@ def scan_smtps_port(host):
     else:
         response_data['state'] = 'Closed or filtered.'
 
-def scan_ldap_port(host):
-    port = 636
+def scan_ldap_port(host, port):
+    #port = 636
     response_data = {
         'port': port,
         'state': 'closed'
@@ -151,8 +158,8 @@ def scan_ldap_port(host):
     
     return response_data
 
-def scan_ldaps_port(host):
-    port = 389
+def scan_ldaps_port(host, port):
+    #port = 389
     response_data = {
         'port': port,
         'state': 'closed'
@@ -177,8 +184,8 @@ def scan_ldaps_port(host):
     
     return response_data
 
-def Telnet_scan(host):
-    port = 23
+def Telnet_scan(host, port):
+    #port = 23
     service_name = "Telnet"
     
     response_data = {
@@ -202,8 +209,8 @@ def Telnet_scan(host):
         #return {'port': port, 'status': 'error', 'service_name': service_name, 'banner': None}  # 그 외 예외 발생 시
     return response_data
 
-def SMTP_scan(host):
-    port = 25
+def SMTP_scan(host, port):
+    #port = 25
     service_name = "SMTP"
     response_data = {
         'port': port,
@@ -228,8 +235,8 @@ def SMTP_scan(host):
         #return {'port': port, 'status': 'error', 'service_name': service_name, 'banner': None}  # 예외 발생 시
     return response_data
 
-def DNS_scan(host):
-    port = 53
+def DNS_scan(host, port):
+    #port = 53
     response_data = {
         'port': port,
         'state': 'closed'
@@ -255,8 +262,8 @@ def DNS_scan(host):
     return response_data
 
 
-def port123_ntp(host, timeout=1):
-    port = 123
+def port123_ntp(host, port, timeout=1):
+    #port = 123
     message = '\x1b' + 47 * '\0'
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(timeout)
@@ -282,7 +289,7 @@ def port123_ntp(host, timeout=1):
     }
     return response_data
 
-def port445_smb(host, timeout=1):
+def port445_smb(host, port, timeout=1):
     response_data = {}
     connection = Connection(uuid.uuid4(), host, 445)
     connection.connect(timeout=timeout)
@@ -294,7 +301,7 @@ def port445_smb(host, timeout=1):
     connection.disconnect()
     return response_data
 
-def port902_vmware_soap(host, port=902, timeout=1):
+def port902_vmware_soap(host, port, timeout=1):
     response_data = {'port': port, 'status': 'closed'} 
 
     try:
@@ -336,8 +343,8 @@ def port902_vmware_soap(host, port=902, timeout=1):
 
     return response_data
 
-def port3306_mysql(host, timeout=1):
-    port = 3306
+def port3306_mysql(host, port, timeout=1):
+    #port = 3306
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(timeout)
     s.connect((host, port))
@@ -361,11 +368,8 @@ def port3306_mysql(host, timeout=1):
         return response_data
     
 
-def IMAP_conn(host):
-    
+def IMAP_conn(host, port):
     host = "outlook.office365.com" #임시로 설정
-    port = 143
-    #host = "imap.gmail.com"
     
     response_data = {
         'port': port,
@@ -374,16 +378,16 @@ def IMAP_conn(host):
     }
     
     try:
-        imap_server = imaplib.IMAP4(host, port)
-                
+        if port == 993:
+            imap_server = imaplib.IMAP4_SSL(host,port)
+        else:
+            imap_server = imaplib.IMAP4(host,port)
         # 배너정보 가져오기
         banner_info = imap_server.welcome
         response_data['status'] = 'open'
         response_data['banner'] = banner_info
         
         # 디코딩 과정 원래 있었는데 생략
-               
-        
     except imaplib.IMAP4.error as imap_error:
         #print("IMAP 오류:", imap_error)
         response_data['status'] = 'error'
@@ -396,48 +400,10 @@ def IMAP_conn(host):
         response_data['error_message'] = str(e)
         
     return response_data
-        
-        
-def IMAPS_conn(host):
-    host = "outlook.office365.com" #임시로 설정
-    port = 993
-    
-    response_data = {
-        'port': port,
-        'status': 'closed',
-        'banner': None,
-    }
-    
-    try:
-        # IMAP 서버에 SSL 연결 설정
-        imap_server = imaplib.IMAP4_SSL(host, port)
-                
-        # 배너정보 가져오기
-        banner_info = imap_server.welcome
-        banner_info = imap_server.welcome
-        response_data['status'] = 'open'
-        response_data['banner'] = banner_info
-        
-            
-    except imaplib.IMAP4_SSL.error as ssl_error:
-        #print("SSL error:", ssl_error)
-        response_data['error_message'] = ssl_error
-    
-    except imaplib.IMAP4.error as imap_error:
-        response_data['status'] = 'error'
-        response_data['error_message'] = imap_error
-
-    except Exception as e:
-        #print(f"{port}포트 \n예기치 않은 오류 발생\n{e}\n")
-        response_data['status'] = 'error'
-        response_data['error_message'] = str(e)
-        
-        
-    return response_data
 
 #승희님 161    
-def SNMP_conn(host):
-    port = 161
+def SNMP_conn(host, port):
+    #port = 161
     community = 'public'
     host = '192.168.0.35' # 가상머신 서버
     
@@ -494,8 +460,8 @@ def SNMP_conn(host):
 
 
 #영창님 21
-def scan_ftp_port(host):
-    port = 21 #ftp 포트
+def scan_ftp_port(host, port):
+    #port = 21 #ftp 포트
     response_data = {
         'port': port,
         'status': 'closed',
@@ -530,8 +496,8 @@ def scan_ftp_port(host):
 
 
 #영창님 22
-def scan_ssh_port(host):
-    port = 22 #ssh 포트
+def scan_ssh_port(host, port):
+    #port = 22 #ssh 포트
     response_data = {
         'port': port,
         'status': 'closed',
